df406423bb95af2f13391243a71b92ec80adfc44
0xPhilipp <philipp@erisprotocol.com>
2023-02-22 23:43:43 +0100

    add release scripts

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -11,7 +11,17 @@ mkdir -p "../../$folderName-cache"
 mkdir -p "../../$folderName-cache/target"
 mkdir -p "../../$folderName-cache/registry"
 
-docker run --rm -v "/$projectPath":/code \
+
+if [ "$1" == "TESTNET" ]
+then
+  echo "Applying TESTNET" 
+  sed -i 's/WEEK: u64 = 7 \* 86400/WEEK: u64 = 60 \* 60/' "$projectPath/packages/eris/src/governance_helper.rs"
+else
+  echo "Applying $1"
+  sed -i 's/WEEK: u64 = 60 \* 60/WEEK: u64 = 7 \* 86400/' "$projectPath/packages/eris/src/governance_helper.rs"
+fi
+
+docker run --env $1 --rm -v "/$projectPath":/code \
   --mount type=bind,source=/$projectPath-cache/target,target=/code/target \
   --mount type=bind,source=/$projectPath-cache/registry,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.6
+  cosmwasm/workspace-optimizer:0.12.6 
