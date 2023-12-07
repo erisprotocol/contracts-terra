51ce937810f48fc55bc066d3c1b8505439bd5dc0
0xPhilipp <philipp@erisprotocol.com>
2023-12-07 09:16:27 +0100

    fix version and contract name

--- contracts/hub/src/constants.rs
+++ contracts/hub/src/constants.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::Decimal;
 
-pub const CONTRACT_NAME: &str = "eris-staking-hub-archway";
+pub const CONTRACT_NAME: &str = "eris-staking-hub-juno";
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub fn get_reward_fee_cap() -> Decimal {
